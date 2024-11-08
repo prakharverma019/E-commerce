34331615133b84fbe24e03f61b8e3c979e8010e4
prakhar verma <prahkarvermawork@gmail.com>
2024-11-09 03:10:33 +0530

    delelte link

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,5 +1,4 @@
-
-export const cart =[{
+export let cart =[{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 },{
@@ -24,4 +23,8 @@ export function addToCart(productId) {
         quantity
       })
     }
+}
+
+export function removeFromCart(productId) {
+  cart = cart.filter((cartItem) => cartItem.productId !== productId);
 }
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from "../data/cart.js";
+import {cart, removeFromCart} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -35,7 +35,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingItem.id}">
                     Delete
                   </span>
                 </div>
@@ -55,7 +55,7 @@ cart.forEach((cartItem) => {
                     </div>
                     <div class="delivery-option-price">
                       FREE Shipping
-                    </div>
+                    </div>f
                   </div>
                 </div>
                 <div class="delivery-option">
@@ -88,6 +88,14 @@ cart.forEach((cartItem) => {
             </div>
           </div>`;
           document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+          document.querySelectorAll('.js-delete-link').forEach((link)=>{
+            link.addEventListener("click", ()=>{
+              const productId = link.dataset.productId;
+              removeFromCart(productId);
+            })
+
+          })
 });
 
     
